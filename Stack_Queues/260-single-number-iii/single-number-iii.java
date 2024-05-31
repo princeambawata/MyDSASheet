e3240fa2e48867cdf5d5bef4a73a30399495a89e
Prince Kumar <veerambawata@gmail.com>
2024-05-31 22:20:09 +0530

    Time: 1 ms (100.00%) | Memory: 45.3 MB (90.35%) - LeetSync

--- Stack_Queues/260-single-number-iii/single-number-iii.java
+++ Stack_Queues/260-single-number-iii/single-number-iii.java
@@ -1,18 +1,37 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        Set<Integer> s = new HashSet<>();
+        // Set<Integer> s = new HashSet<>();
+        // int n = nums.length;
+        // for(int i=0;i<n;i++){
+        //     if(!s.contains(nums[i])){
+        //         s.add(nums[i]);
+        //     }else{
+        //         s.remove(nums[i]);
+        //     }
+        // }
+        // int[] ans = new int[2];
+        // int k=0;
+        // for(Integer i : s){
+        //     ans[k++] = i;
+        // }
+        // return ans;
+
         int n = nums.length;
-        for(int i=0;i<n;i++){
-            if(!s.contains(nums[i])){
-                s.add(nums[i]);
-            }else{
-                s.remove(nums[i]);
-            }
+        int xor = 0;
+        for(int i : nums){
+            xor ^= i;
         }
+        int rmsb = xor & (-1*xor);
         int[] ans = new int[2];
-        int k=0;
-        for(Integer i : s){
-            ans[k++] = i;
+        ans[0] = 0;
+        ans[1] = 0;
+        for(int num : nums){
+            int x = rmsb & num;
+            if(x == 0){
+                ans[0] ^=  num;
+            }else{
+                ans[1] ^= num;
+            }
         }
         return ans;
     }
