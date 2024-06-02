babad4be51730ac755c1a74c8e146737622b1438
Prince Kumar <veerambawata@gmail.com>
2024-06-02 10:00:52 +0530

    Time: 40 ms (29.27%) | Memory: 46.8 MB (28.46%) - LeetSync

--- Stack_Queues/3451-string-compression-iii/string-compression-iii.java
+++ Stack_Queues/3451-string-compression-iii/string-compression-iii.java
@@ -2,29 +2,7 @@ class Solution {
     public String compressedString(String word) {
         Map<Character, Integer> m = new LinkedHashMap<>();
         int n = word.length();
-        // for(int i=0;i<n;i++){
-        //     char c = word.charAt(i);
-        //     if(m.containsKey(c)){
-        //         m.put(c, m.get(c)+1);
-        //     }else{
-        //         m.put(c, 1);
-        //     }
-        // }
         StringBuilder s = new StringBuilder();
-        // for(Map.Entry<Character, Integer> e : m.entrySet()){
-        //     int val = e.getValue();
-        //     while(val>0){
-        //         if(val > 9){
-        //             String st = "" + 9 + e.getKey();
-        //             s.append(st);
-        //             val = val-9;
-        //         }else{
-        //             String st = "" + val + e.getKey();
-        //             s.append(st);
-        //             val = 0;
-        //         }
-        //     }
-        // }
         int count = 1;
         for(int i=1;i<n;i++){
             if(word.charAt(i) == word.charAt(i-1)){
