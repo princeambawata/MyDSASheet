c4a7258f68d36414765a4b6aa0d6c3c8033655fe
Prince Kumar <veerambawata@gmail.com>
2024-06-11 23:02:02 +0530

    Time: 2 ms (58.42%) | Memory: 44.2 MB (96.60%) - LeetSync

--- Stack_Queues/152-maximum-product-subarray/maximum-product-subarray.java
+++ Stack_Queues/152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,5 +1,21 @@
 class Solution {
     public int maxProduct(int[] nums) {
+        // int max_prod = 1;
+        // int min_prod = 1;
+        // int ans = Integer.MIN_VALUE;
+        // int n = nums.length;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i] > 0){
+        //         max_prod = Math.max(max_prod*nums[i], nums[i]);
+        //         min_prod = Math.min(max_prod*nums[i], nums[i]);
+        //     }else{
+        //         int temp = max_prod;
+        //         max_prod = Math.max(min_prod*nums[i], nums[i]);
+        //         min_prod = Math.min(temp*nums[i], nums[i]);
+        //     }
+        //     ans = Math.max(ans, max_prod);
+        // }
+        // return ans;
         int n = nums.length;
         int currMax = 1;
         int currMin = 1;
