521360b428a5ea47ecc3042de82a1eba1adca4b1
Prince Kumar <veerambawata@gmail.com>
2024-06-11 21:33:10 +0530

    Time: 3 ms (68.00%) | Memory: 45 MB (95.31%) - LeetSync

--- Stack_Queues/64-minimum-path-sum/minimum-path-sum.java
+++ Stack_Queues/64-minimum-path-sum/minimum-path-sum.java
@@ -1,21 +1,20 @@
 class Solution {
-
-    public int solve(int[][] grid, int m, int n, int[][] dp){
-        if(m<0 || n<0) return Integer.MAX_VALUE;
-        if(m ==0 && n ==0) return grid[0][0];
-        else if(dp[m][n] != -1) return dp[m][n];
-        return dp[m][n] = Math.min(solve(grid, m-1, n, dp), solve(grid, m, n-1, dp)) + grid[m][n];
-    }
-
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n  = grid[0].length;
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j] = -1;
+        int n = grid.length;
+        int m = grid[0].length;
+        int dp[][] = new int[n][m];
+        dp[0][0] = grid[0][0];
+        for(int i=1;i<m;i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        for(int i=1;i<n;i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
             }
         }
-        return solve(grid, m-1, n-1, dp);
+        return dp[n-1][m-1];
     }
 }
