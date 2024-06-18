a8a0564a4326c1b5d1c252ba9645b288be0beb0a
Prince Kumar <veerambawata@gmail.com>
2024-06-18 22:58:54 +0530

    Time: 10 ms (25.76%) | Memory: 48.2 MB (10.73%) - LeetSync

--- Stack_Queues/518-coin-change-ii/coin-change-ii.java
+++ Stack_Queues/518-coin-change-ii/coin-change-ii.java
@@ -1,26 +1,22 @@
 class Solution {
-
-    public int solve(int coins[], int n, int amount, int[][] dp){
-        if(n == 0) return (amount%coins[0]) == 0 ? 1 : 0;
-        else if(amount == 0) return 1; 
-        else if(dp[n][amount] != -1) return dp[n][amount];
-        int notTake = solve(coins, n-1, amount, dp);
-        int take = 0;
-        if(amount-coins[n] >= 0){
-            take = solve(coins, n, amount-coins[n], dp);
-        }
-        return dp[n][amount] = notTake+take;
-    }
-
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                dp[i][j] = -1;
+        int dp[][] = new int[n][amount+1];
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0] == 0) {
+                dp[0][i] = 1;
             }
         }
-        int ans = solve(coins, n-1, amount, dp);
-        return ans;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int notTake = dp[i-1][j];
+                int take = 0;
+                if(j-coins[i] >= 0){
+                    take = dp[i][j-coins[i]];
+                }
+                dp[i][j] = take + notTake;
+            }
+        }
+        return dp[n-1][amount];
     }
 }
