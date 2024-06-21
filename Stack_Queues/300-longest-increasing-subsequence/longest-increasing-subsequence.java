5598082a01153db4b6ad8bcf66888f8d446f78ae
Prince Kumar <veerambawata@gmail.com>
2024-06-21 22:16:27 +0530

    Time: 40 ms (66.84%) | Memory: 43.4 MB (98.24%) - LeetSync

--- Stack_Queues/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ Stack_Queues/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,39 +1,22 @@
 class Solution {
 
-    public int solve(int[] nums, int index, int n, int prevI,int[][] dp){
-        if(index == n) return 0;
-        if(dp[index][prevI+1] != -1) return dp[index][prevI+1];
-        int notTake = solve(nums, index+1, n, prevI, dp);
-        int take = 0;
-        if(prevI == -1 || nums[index] > nums[prevI]){
-            take = solve(nums, index+1, n, index, dp)+1;
-        }
-        return dp[index][prevI+1] = Math.max(take, notTake);
-    }
-
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        // int dp[][] = new int[n][n+1];
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<=n;j++){
-        //         dp[i][j] = -1;
-        //     }
-        // }
-
-        // return solve(nums, 0, n, -1, dp);
-
         int dp[] = new int[n];
         for(int i=0;i<n;i++){
             dp[i] = 1;
         }
-        int ans = 1;
-        for(int i=0;i<n;i++){
-            for(int prevI = 0;prevI<i;prevI++){
-                if(nums[prevI] < nums[i]){
-                    dp[i] = Math.max(dp[i], dp[prevI] + 1);
-                    ans = Math.max(ans, dp[i]);
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
                 }
             }
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans = Math.max(ans, dp[i]);
         }
         return ans;
     }
